6d72f9226673d55bfd6a113f835aa4aa8efd2d05
Majid Kareem <43646844+Emkay860@users.noreply.github.com>
2020-04-30 09:00:17 +0100

    Added condition render for background image

--- src/App.js
+++ src/App.js
@@ -54,7 +54,15 @@ export default function App() {
   };
 
   return (
-    <div className="app">
+    <div
+      className={
+        typeof weather.main !== "undefined"
+          ? weather.main.temp > 16
+            ? "app-warm"
+            : "app"
+          : "app"
+      }
+    >
       <main>
         <div className="search-box">
           <input
@@ -66,7 +74,7 @@ export default function App() {
             onKeyPress={search}
           />
         </div>
-        {typeof weather.main != "undefined" ? (
+        {typeof weather.main !== "undefined" ? (
           <div>
             <div className="location-box">
               <div className="location">
@@ -75,8 +83,8 @@ export default function App() {
               <div className="date">{dateBuilder(new Date())} </div>
             </div>
             <div className="weather-box">
-              <div className="temp">15°C</div>
-              <div className="weather"> Sunny </div>
+              <div className="temp">{Math.round(weather.main.temp)}°C</div>
+              <div className="weather"> {weather.weather[0].main} </div>
             </div>
           </div>
         ) : (
